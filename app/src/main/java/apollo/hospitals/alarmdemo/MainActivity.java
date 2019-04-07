97e90f709d0ba92d1d26c79602ce1c9043692233
rohit <rohitmaria100@gmail.com>
2019-04-07 19:53:02 +0530

    Initial commit

---
+++ app/src/main/java/apollo/hospitals/alarmdemo/MainActivity.java
@@ -0,0 +1,39 @@
+package apollo.hospitals.alarmdemo;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Calendar cal = Calendar.getInstance();
+        //cal.add(Calendar.SECOND, 10);
+        cal.add(Calendar.MINUTE,1);
+
+        //Create a new PendingIntent and add it to the AlarmManager
+        Intent intent = new Intent(this, AlarmReceiverActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                12345, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        AlarmManager am =
+                (AlarmManager)getSystemService(Activity.ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                pendingIntent);
+
+    }
+
+}
